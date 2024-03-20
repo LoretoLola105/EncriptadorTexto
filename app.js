4c71df250ed2d6a388da0bc40fb1a1a9bc822de9
Loreto Vlaenzuel Flores <valenzuelaloreto485@gmail.com>
2024-03-20 13:10:08 -0300

    primera version delDdesafio 1 Encripador de textos

---
+++ app.js
@@ -0,0 +1,62 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      muñeco.src = "./img/encriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      let mensajeDiv = document.getElementById("mensaje");
+      mensajeDiv.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        let mensajeDiv = document.getElementById("mensaje");
+        mensajeDiv.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+}
+
+function copiarTexto() {
+    var contenido = document.getElementById("texto").value;
+    navigator.clipboard.writeText(contenido)
+      .then(() => {
+        console.log("Texto copiado al portapapeles: " + contenido);
+        alert("Texto copiado al portapapeles");
+      })
+      .catch(err => {
+        console.error("Error al copiar el texto: ", err);
+        alert("Error al copiar el texto");
+      });
+  }
